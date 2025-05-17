4c2a6a8cd2a9190f90ffc2afb97694a190cce87e
Het Gala <het@Hets-MacBook-Air.local>
2025-05-17 19:53:30 +0530

    Minor improvements from field part 1

--- src/components/BannerImage.jsx
+++ src/components/BannerImage.jsx
@@ -16,15 +16,15 @@ const BannerImage = ({ image, title, fromText, fromLink, toText, }) => {
         <div className="absolute inset-0 bg-white opacity-20"></div>
         
         <div className="relative z-10 h-full flex flex-col justify-center items-center text-white p-2 sm:p-4">
-          <div className="border-b-2 border-dashed text-lg sm:text-2xl pb-2 font-medium">R J Gala & Associates</div>
+          <div className="border-b-2 border-dashed text-lg sm:text-5xl pb-2 font-medium">R J Gala & Associates</div>
           <h2 className="text-xl text-center sm:text-3xl md:text-5xl hover:text-custom-secondary text-custom-secondary font-extrabold my-3 sm:my-5">{ title }</h2>
-<div className="flex flex-row items-center justify-center gap-1 sm:gap-4 text-center">
-  <Link className="text-base sm:text-lg hover:text-custom-secondary p-1 hover:cursor-pointer" to={ fromLink }>
-    { fromText }
-  </Link>
-  <ImArrowRight2 size={16} className="mx-1 sm:mx-0" />
-  <div className="text-base sm:text-lg hover:text-custom-secondary p-1">{ toText }</div>
-</div>
+          <div className="flex flex-row items-center justify-center gap-1 sm:gap-4">
+            <Link className="text-base sm:text-lg text-center hover:text-custom-secondary p-1 hover:cursor-pointer" to={ fromLink }>
+              { fromText }
+            </Link>
+            <ImArrowRight2 size={16} className="mx-1 sm:mx-0" />
+            <div className="text-base sm:text-lg text-center hover:text-custom-secondary p-1">{ toText }</div>
+          </div>
         </div>
       </div>
     </div>
